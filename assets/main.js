b369cd657ab2e95e24e13b634e2ecfac951819d0
henrique-roldao <henriqueroldao@rede.ulbra.br>
2021-07-06 11:25:32 -0300

    Javascript para capturar as APi's pronto

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+const text = document.querySelector("#text");
+const imagem = document.querySelector("#img");
+
+async function requestMessage() {
+  const requestMessage = await fetch("https://api.adviceslip.com/advice");
+  const dataMessage = await requestMessage.json();
+  const message = dataMessage.slip.advice;
+
+  text.innerHTML = `<p>${message}</p>`;
+}
+
+async function requestDog() {
+  const response = await fetch("https://random.dog/woof.json");
+  const data = await response.json();
+  const dataUrl = data.url;
+
+  const validation = dataUrl.split(".mp4", 2);
+  if (validation.length == 2) {
+    requestDog();
+  } else {
+    imagem.innerHTML = `<img src="${dataUrl}" alt="Doguinho aleatório">`;
+  }
+}
+
+requestMessage();
+requestDog();
